6d979f239cc19a61334d84b8c7eaddfb5425b10e
James Zhou <jameszhou@Jamess-MacBook-Air.local>
2025-11-07 23:31:17 -0500

    added ocr

--- apps/backend/server.py
+++ apps/backend/server.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
     import uvicorn
     
     uvicorn.run(
-        "api:app",
+        "server:app",
         host="0.0.0.0",
         port=8000,
         reload=True
